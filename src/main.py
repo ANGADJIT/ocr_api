3bed44d2a501f9e7e9419c3b9333b9a921cdf62b
Angadjit singh <angad38.agcs@gmail.com>
2023-04-03 14:15:35 +0530

    added categorization logic

--- src/main.py
+++ src/main.py
@@ -1,23 +1,37 @@
-from fastapi import FastAPI,UploadFile,File
-
+from fastapi import FastAPI, UploadFile, File
 import keras_ocr
-import json
+from text_category_function import categorize_texts
+# from skimage.io import imsave
+from fastapi.responses import FileResponse
 
-app=FastAPI()
+app = FastAPI()
 pipeline = keras_ocr.pipeline.Pipeline()
 
-@app.post("/get_image")
-def get_image(image: UploadFile = File(...)):
-     
-     img = keras_ocr.tools.read(image.file) 
-     prediction_groups = pipeline.recognize([img])
-     prediction_object=[(text,boxes.tolist()) for text, boxes in prediction_groups[0]]
-     only_text=[text[0] for text in prediction_object ]
-     only_boxes=[box[1] for box in prediction_object[0] ]
-     return {
-          'text':only_text,
-          'boxes':only_boxes
-     }
-    		
 
-     
+@app.post("/process_image")
+def get_image(image: UploadFile = File(...)):
+
+    img = keras_ocr.tools.read(image.file)
+
+    prediction_groups = pipeline.recognize([img])
+    prediction_object = [(text, boxes.tolist())
+                         for text, boxes in prediction_groups[0]]
+    only_text = [text[0] for text in prediction_object]
+
+    categorized_text: list = categorize_texts(only_text)
+
+#     output_image = keras_ocr.tools.drawBoxes(
+#         image=img, boxes=prediction_groups[0])
+
+#     if output_image is not None:
+#         imsave('assets/output.jpg', output_image)
+
+    return {
+        'categorized_text': categorized_text
+    }
+
+
+@app.get('/get_image/{image_key}')
+def get_image(image_key: str):
+
+    return FileResponse(path='assets/img.png')
